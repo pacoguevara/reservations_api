45b91f46b1a5aaae22e98c28710afd506fe00997
Paco Guevara <pacoguevaraa@gmail.com>
2021-11-06 01:35:41 -0700

    Create method to validate timeslots

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,4 +7,16 @@ class Reservation < ApplicationRecord
   validates_presence_of :end_time
 
   accepts_nested_attributes_for :vehicle
+
+  def valid_timeslot?
+    reservations = Reservation.where(start_time: self.start_time.midnight..self.start_time.end_of_day)
+                              .where.not(id: self.id)
+                              .order(:start_time)
+    reservations.each do |reservation|
+      start_arr = [self.start_time, reservation.start_time]
+      end_arr = [self.end_time, reservation.end_time]
+      return false if start_arr.max < end_arr.min
+    end
+    true
+  end
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -11,5 +11,49 @@ RSpec.describe Reservation, type: :model do
     it "validate relationships" do
       should belong_to(:vehicle)
     end
+
+    it "should return true if a timeslot is valid" do
+      customer = create(:customer)
+      service = create(:service)
+      vehicle = create(:vehicle, customer: customer)
+      create(:reservation,
+              service: service,
+              vehicle: vehicle,
+              start_time: DateTime.new(2021, 11, 5, 12, 0, 0),
+              end_time: DateTime.new(2021, 11, 5, 12, 59, 59))
+      create(:reservation,
+              service: service,
+              vehicle: vehicle,
+              start_time: DateTime.new(2021, 11, 5, 14, 0, 0),
+              end_time: DateTime.new(2021, 11, 5, 14, 59, 59))
+      reservation = create(:reservation,
+                            service: service,
+                            vehicle: vehicle,
+                            start_time: DateTime.new(2021, 11, 5, 13, 0, 0),
+                            end_time: DateTime.new(2021, 11, 5, 13, 59, 59))
+      expect(reservation.valid_timeslot?).to eq true
+    end
+
+    it "should return false if a timeslot is not valid" do
+      customer = create(:customer)
+      service = create(:service)
+      vehicle = create(:vehicle, customer: customer)
+      create(:reservation,
+              service: service,
+              vehicle: vehicle,
+              start_time: DateTime.new(2021, 11, 5, 12, 0, 0),
+              end_time: DateTime.new(2021, 11, 5, 12, 59, 59))
+      create(:reservation,
+              service: service,
+              vehicle: vehicle,
+              start_time: DateTime.new(2021, 11, 5, 13, 0, 0),
+              end_time: DateTime.new(2021, 11, 5, 14, 30, 59))
+      reservation = create(:reservation,
+                            service: service,
+                            vehicle: vehicle,
+                            start_time: DateTime.new(2021, 11, 5, 12, 30, 0),
+                            end_time: DateTime.new(2021, 11, 5, 13, 59, 59))
+      expect(reservation.valid_timeslot?).to eq false
+    end
   end
 end
